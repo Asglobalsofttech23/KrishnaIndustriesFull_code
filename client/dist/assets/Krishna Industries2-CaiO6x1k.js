9d3ecec4eeacd9a34f10df51f9bef7c3a39c126d
sivaHarish02 <sivaharish0202@gmail.com>
2024-08-19 12:41:44 +0530

    fullcode

---
+++ client/dist/assets/Krishna Industries2-CaiO6x1k.js
@@ -0,0 +1,1 @@
+const e=""+new URL("Krishna Industries2-d-0GPqQG.jpeg",import.meta.url).href;export{e as l};
--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,8 @@
 const config = {
   // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
   // like '/berry-material-react/react/default'
-  // apiUrl : `https://krishnaindustries.onrender.com`,
-  apiUrl : `http://localhost:2002`,
+  apiUrl : `https://backend.krishnaindustries.cloud`,
+  // apiUrl : `http://localhost:2002`,
   basename: '',
   defaultPath: '/dashboard/default',
   fontFamily: `'Roboto', sans-serif`,
